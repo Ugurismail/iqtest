9ae4da93f7711ddad4d69ea92b5f6ba3a141de51
Ugurismail <uguraygun001@hotmail.com>
2022-11-27 13:59:55 +0300

    Turkish Questions Ok

--- script.js
+++ script.js
@@ -17,26 +17,26 @@ const english = {
 		4: ["Dishonesty", "Impoverished", "Significant", "Graceful", "Diffuse"],
 	},
 	ayarlaEng: function () {
-		const random = Math.floor(Math.random() * 5);
-		const soru = document.getElementById("soru");
-		soru.innerText = this.soru;
+		const randomEng = Math.floor(Math.random() * 5);
+		const soruEng = document.getElementById("soruEng");
+		soruEng.innerText = this.soru;
 
 		const eklenecekCümle = `
-    ${this.cümleler[random]}
+    ${this.cümleler[randomEng]}
     `;
 
-		const cumleContainer = document.getElementById("cumle");
+		const cumleContainer = document.getElementById("cumleEng");
 		cumleContainer.insertAdjacentHTML("afterbegin", eklenecekCümle);
 
-		const eklenecekSiklar = `    
-    <h6 class="english" id="english sik" name="a"> A-${this.yanitlar[random][0]}</h6>
-    <h6 class="english" id="english sik" name="b"> B-${this.yanitlar[random][1]}</h6>
-    <h6 class="english" id="english sik" name="c"> C-${this.yanitlar[random][2]}</h6>
-    <h6 class="english" id="english sik" name="d"> D-${this.yanitlar[random][3]}</h6>
-    <h6 class="english" id="english sik" name="e" cami ="cami"> E-${this.yanitlar[random][4]}</h6>`;
+		const eklenecekSiklarEng = `    
+    <h6 class="english" id="english sik" name="a"> A-${this.yanitlar[randomEng][0]}</h6>
+    <h6 class="english" id="english sik" name="b"> B-${this.yanitlar[randomEng][1]}</h6>
+    <h6 class="english" id="english sik" name="c"> C-${this.yanitlar[randomEng][2]}</h6>
+    <h6 class="english" id="english sik" name="d"> D-${this.yanitlar[randomEng][3]}</h6>
+    <h6 class="english" id="english sik" name="e" cami ="cami"> E-${this.yanitlar[randomEng][4]}</h6>`;
 
-		const şıklarContainer = document.getElementById("şıklar");
-		şıklarContainer.insertAdjacentHTML("afterbegin", eklenecekSiklar);
+		const şıklarContainer = document.getElementById("şıklarEng");
+		şıklarContainer.insertAdjacentHTML("afterbegin", eklenecekSiklarEng);
 	},
 };
 
@@ -46,6 +46,69 @@ english.ayarlaEng();
 const englishAnswers = document.querySelectorAll(".english");
 
 englishAnswers.forEach((answer) =>
+	answer.addEventListener("click", (e) => {
+		const yanitdeger = e.target.attributes.name.value;
+		console.log(yanitdeger);
+	}),
+);
+
+// Türkçe sorusu için gerekli durum.
+const turkish = {
+	soru: "Aşağıdaki cümlelerin hangisinde yazım yanlışı yapılmıştır?",
+	yanitlar: {
+		0: [
+			"Rakunum, kulübe de yatıyor.***",
+			"Sabahları, şen düşlerde gezerdim…",
+			"Kulaklarda hep aynı nağme.",
+			"Keşke aklım sende kalmasaydı…",
+			"Yeşil yeleğin artık bende.",
+		],
+		1: [
+			"Rakun, sırtüstü de yüzüyor.",
+			"Sabahları, şen düşler de gezerdim…***",
+			"Kulakların da çok ufakmış.",
+			"Keşke aklım sende kalmasaydı…",
+			"Yeşil yeleğini artık ben de giyerim.",
+		],
+		2: [
+			"Rakunum, kulübesinde yatıyor.",
+			"Sabahları, şen düşlerde gezerdim…",
+			"Kulaklarda hep aynı nağme.",
+			"Sende benim kadar gerçekleri görüyorsun.**",
+			"Yeşil yeleğin artık bende.",
+		],
+		3: [
+			"Rakunum, kulübesinde yatıyor.",
+			"Sabahları, şen düşlerde gezerdim…",
+			"Gözlerinde bozukmuş senin.**",
+			"Keşke aklım sende kalmasaydı…",
+			"Yeşil yeleğin artık bende.",
+		],
+		4: [
+			"Rakunum, kulübesinde yatıyor.",
+			"Sabahları, şen düşlerde gezerdim…",
+			"Kulaklarda hep aynı nağme.",
+			"Keşke aklım sende kalmasaydı…",
+			"Yeşil yeleğin artık ben de.*",
+		],
+	},
+};
+document.getElementById("soruTr").innerText = turkish.soru;
+const randomTr = Math.floor(Math.random() * 5);
+const eklenecekSiklarTr = `    
+<h6 class="turkish" id="turkish sik" name="a"> A-${turkish.yanitlar[randomTr][0]}</h6>
+<h6 class="turkish" id="turkish sik" name="b"> B-${turkish.yanitlar[randomTr][1]}</h6>
+<h6 class="turkish" id="turkish sik" name="c"> C-${turkish.yanitlar[randomTr][2]}</h6>
+<h6 class="turkish" id="turkish sik" name="d"> D-${turkish.yanitlar[randomTr][3]}</h6>
+<h6 class="turkish" id="turkish sik" name="e"> E-${turkish.yanitlar[randomTr][4]}</h6>`;
+
+document
+	.getElementById("şıklarTr")
+	.insertAdjacentHTML("afterbegin", eklenecekSiklarTr);
+
+const turkishAnswers = document.querySelectorAll(".turkish");
+
+turkishAnswers.forEach((answer) =>
 	answer.addEventListener("click", (e) => {
 		const yanitdeger = e.target.attributes.name.value;
 		console.log(yanitdeger);
